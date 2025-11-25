64b9b1b285df8771b0b433a2ef6aa93802dc6b5c
PrabhuLoganathan <30402384+PrabhuLoganathan@users.noreply.github.com>
2025-11-25 23:47:25 +0530

    Updated

--- create_venv.sh
+++ create_venv.sh
@@ -2,8 +2,8 @@
 set -e
 PYTHON_BIN="${PYTHON_BIN:-python3}"
 echo "Using $PYTHON_BIN"
-$PYTHON_BIN -m venv venv
-source venv/bin/activate
+$PYTHON_BIN -m venv .venv
+source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 echo "✅ venv ready. Activate with: source venv/bin/activate"
